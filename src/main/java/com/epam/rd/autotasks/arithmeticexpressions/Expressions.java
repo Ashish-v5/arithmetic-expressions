8b92646728e623d18f12652b81545ad7ff8c5bdb
Evgenii_Efimchik <evgenii_efimchik@epam.com>
2022-02-09 19:25:55 +0300

    Initial Commit

---
+++ src/main/java/com/epam/rd/autotasks/arithmeticexpressions/Expressions.java
@@ -0,0 +1,37 @@
+package com.epam.rd.autotasks.arithmeticexpressions;
+
+import java.util.StringJoiner;
+
+public class Expressions {
+
+    public static Variable var(String name, int value) {
+        //implement this method
+        return null;
+    }
+
+    public static Expression val(int value) {
+        //implement this method
+        return null;
+    }
+
+    public static Expression sum(Expression... members){
+        //implement this method
+        return null;
+    }
+
+    public static Expression product(Expression... members){
+        //implement this method
+        return null;
+    }
+
+    public static Expression difference(Expression minuend, Expression subtrahend){
+        //implement this method
+        return null;
+    }
+
+    public static Expression fraction(Expression dividend, Expression divisor){
+        //implement this method
+        return null;
+    }
+
+}
---
+++ src/main/java/com/epam/rd/autotasks/arithmeticexpressions/Variable.java
@@ -0,0 +1,24 @@
+package com.epam.rd.autotasks.arithmeticexpressions;
+
+public class Variable implements Expression {
+
+    public Variable(String name, int value) {
+        //implement this method
+    }
+
+    public void setValue(int value) {
+        //implement this method
+    }
+
+    @Override
+    public int evaluate() {
+        //implement this method
+        return 0;
+    }
+
+    @Override
+    public String toExpressionString() {
+        //implement this method
+        return null;
+    }
+}
