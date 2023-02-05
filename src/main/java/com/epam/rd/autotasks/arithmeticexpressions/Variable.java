af1e910c6534a18636b32f0367c98a9a51cc73ca
Ashish7429 <ashish.11916604@lpu.in>
2023-02-05 19:56:13 +0530

    dfgh

--- src/main/java/com/epam/rd/autotasks/arithmeticexpressions/Variable.java
+++ src/main/java/com/epam/rd/autotasks/arithmeticexpressions/Variable.java
@@ -1,24 +1,25 @@
 package com.epam.rd.autotasks.arithmeticexpressions;
 
 public class Variable implements Expression {
+    public String name;
+    public int value;
 
     public Variable(String name, int value) {
-        //implement this method
+        this.value = value;
+        this.name = name;
     }
 
     public void setValue(int value) {
-        //implement this method
+        this.value = value;
     }
 
     @Override
     public int evaluate() {
-        //implement this method
-        return 0;
+        return value;
     }
 
     @Override
     public String toExpressionString() {
-        //implement this method
-        return null;
+        return name;
     }
 }
